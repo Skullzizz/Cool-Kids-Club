f6a14def4cdb6c6f75736e14640cd2844792b7c5
Deven Wyckoff <justdeven@hotmail.com>
2025-08-27 20:07:02 -0400

    Fix implementation of inventory mechanics

--- GameDev2Project/Assets/Scripts/gamemanager.cs
+++ GameDev2Project/Assets/Scripts/gamemanager.cs
@@ -24,6 +24,7 @@ public class gamemanager : MonoBehaviour
     public GameObject player;
     public playerController playerScript;
     public throwPhysics throwScript;
+    public playerInventory playerInventory;
 
     public bool isPaused;
 
