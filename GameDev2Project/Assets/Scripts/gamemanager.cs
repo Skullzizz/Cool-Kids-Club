8a64f9a4f90ef80fa099a2ccf0b62cff091196b9
Karlynn <karlynnc70@gmail.com>
2025-08-27 20:02:58 -0500

    Checkpoint/Spawn

--- GameDev2Project/Assets/Scripts/gamemanager.cs
+++ GameDev2Project/Assets/Scripts/gamemanager.cs
@@ -24,6 +24,7 @@ public class gamemanager : MonoBehaviour
     public GameObject player;
     public playerController playerScript;
     public throwPhysics throwScript;
+    public GameObject playerSpawnPos;
 
     public bool isPaused;
 
@@ -45,6 +46,8 @@ public class gamemanager : MonoBehaviour
         player = GameObject.FindWithTag("Player");
         playerScript = player.GetComponent<playerController>();
         throwScript = player.GetComponent<throwPhysics>();
+
+        playerSpawnPos = GameObject.FindWithTag("Player Spawn");
     }
 
     // Update is called once per frame
