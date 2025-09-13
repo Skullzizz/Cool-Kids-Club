2611c695648393c91f1f0e1a6ec81653ace5f366
Rya-Nel <81937163+Rya-Nel@users.noreply.github.com>
2025-09-13 17:48:34 -0400

    UI
    -Main Menu Scene
    - Options Screen
    -Credit Screen
    -Music added to menu
        - options to change music and SFX volume
    -options to change sensitivity

---
+++ GameDev2Project/Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class AudioSettings : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public string musicParam = "MusicVolume";
+    public string sfxParam = "SFXVolume";
+
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    private const string KeyMusic = "MusicVolume";
+    private const string KeySFX = "SFXVolume";
+
+    private const float MinLinear = 0.0001f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnEnable()
+    {
+        float music = PlayerPrefs.GetFloat(KeyMusic, 0.8f);
+        float sfx = PlayerPrefs.GetFloat(KeySFX, 0.8f);
+
+        if (musicSlider) musicSlider.SetValueWithoutNotify(music);
+        if (sfxSlider) sfxSlider.SetValueWithoutNotify(sfx);
+
+        SetMusicVolume(music);
+        SetSFXVolume(sfx);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        float dB = Mathf.Log10(Mathf.Max(volume, MinLinear)) * 20f;
+        audioMixer.SetFloat(musicParam, dB);
+        PlayerPrefs.SetFloat(KeyMusic, volume);
+        PlayerPrefs.Save();
+    }
+    public void SetSFXVolume(float volume)
+    {
+        float dB = Mathf.Log10(Mathf.Max(volume, MinLinear)) * 20f;
+        audioMixer.SetFloat(sfxParam, dB);
+        PlayerPrefs.SetFloat(KeySFX, volume);
+        PlayerPrefs.Save();
+    }
+}
