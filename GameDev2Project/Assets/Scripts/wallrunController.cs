2027413d757c1e498791eac9f59ad3b797fb7b91
Deven Wyckoff <justdeven@hotmail.com>
2025-09-14 00:05:12 -0400

    Commit of Exploding Barrel and AOE explosion effect
    Ignore the text mesh stuff, I accidentally imported all of text mesh extras and had to delete and reinstall everything for just the essentials to be there

--- GameDev2Project/Assets/Scripts/wallrunController.cs
+++ GameDev2Project/Assets/Scripts/wallrunController.cs
@@ -167,7 +167,7 @@ public class wallrunController : MonoBehaviour
     // Start Wallrunning
     void startWallrun()
     {
-        if (!ableToWallRun||Water.instance.inWater)
+        if (!ableToWallRun/*||Water.instance.inWater*/)
         { return; }
             if (Input.GetButtonDown("Jump") && (wallLeft || wallRight) && !playerMovement.isGrounded && !isWallRunning && !pController.isCrouching)
             {
