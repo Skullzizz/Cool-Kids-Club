3c431d7543900f2748c70e588733718a0f84f2a7
Deven Wyckoff <justdeven@hotmail.com>
2025-09-21 18:22:07 -0400

    Commit of Debug Removal (Showcase LeveL)

--- GameDev2Project/Assets/Scripts/playerController.cs
+++ GameDev2Project/Assets/Scripts/playerController.cs
@@ -231,10 +231,10 @@ public class playerController : MonoBehaviour, IDamage
 
         controller.Move(playerVel * Time.deltaTime);
 
-        if (Input.GetButtonDown("EnterShowcase"))
-        {
-            EnterShowcaseLevel();
-        }
+        //if (Input.GetButtonDown("EnterShowcase"))
+        //{
+        //    EnterShowcaseLevel();
+        //}
 
         if (Input.GetButton("Fire1") && shootTimer >= shootRate && equippedWeapon != null)
         {
@@ -538,10 +538,10 @@ public class playerController : MonoBehaviour, IDamage
     //    }
     //}
 
-    void EnterShowcaseLevel()
-    {
-        SceneManager.LoadScene("Showcase Level");
-    }
+    //void EnterShowcaseLevel()
+    //{
+    //    SceneManager.LoadScene("Showcase Level");
+    //}
 
     public void Save(ref PlayerData data)
     {
