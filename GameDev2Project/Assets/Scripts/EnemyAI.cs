faab64b21c5632b1c64eb75f4c38cf3e37b4e440
MasonHill243 <MAHill2@student.fullsail.edu>
2025-09-21 17:51:09 -0400

    Audio and FOV Pushes are complete. Some audio is no working

--- GameDev2Project/Assets/Scripts/EnemyAI.cs
+++ GameDev2Project/Assets/Scripts/EnemyAI.cs
@@ -25,6 +25,9 @@ public class EnemyAI : MonoBehaviour, IDamage
     [SerializeField] int roamPauseTime;
     [SerializeField] int animTransSpeed;
 
+  //  [Header("Audio")]
+  //  public AudioSource deathAudio;
+
     [Header("Waypoints")]
     [SerializeField] private Waypoint waypoints;
     [SerializeField] private float waypointThreshold = 0.5f;
@@ -42,6 +45,8 @@ public class EnemyAI : MonoBehaviour, IDamage
     float stoppingDistOrig;
 
     bool playerInTrigger;
+    bool isDead = false;
+
 
     public Vector3 playerDir;
     Vector3 startingPos;
@@ -70,6 +75,10 @@ public class EnemyAI : MonoBehaviour, IDamage
 
         if (waypoints != null)
             currentWaypoint = waypoints.GetNextWaypoint(null);
+
+     //   // death audio
+     //   deathAudio = GetComponent<AudioSource>();
+     //   deathAudio.volume = 0.5f; // Set volume between 0.0 and 1.0
 
     }
 
@@ -104,7 +113,33 @@ public class EnemyAI : MonoBehaviour, IDamage
         {
             playerInTrigger = false;
         }
+
+     //   if (HP <= 0 && !isDead)
+     //   {
+     //       isDead = true;
+     //       StartCoroutine(Die());
+     //   }
+
+
+     //   if (HP <= 0)
+     //   {
+     //       DeathSound();
+     //   }
+
     }
+
+ //  public void DeathSound()
+ //   {
+ //       if (deathAudio != null && !deathAudio.isPlaying)
+ //       {
+ //           deathAudio.Play();
+ //           Destroy(gameObject, deathAudio.clip.length); // Wait for sound to finish
+ //       }
+ //       else
+ //       {
+ //           Destroy(gameObject); // Fallback
+ //       }
+ //   }
 
     void setAnimLoco()
     {
@@ -314,6 +349,14 @@ public class EnemyAI : MonoBehaviour, IDamage
             gamemanager.instance.updateEnemyDeaths(1);
         }
     }
+  //  IEnumerator Die()
+  //  {
+  //      deathAudio.Play();
+  //      yield return new WaitForSeconds(deathAudio.clip.length);
+  //      Destroy(gameObject);
+  //  }
+
+
 
     IEnumerator flashRed()
     {
